67d60064a56f89d55600bda44c199785dd2239ff
Stuart Dilts <stuart.dilts@gmail.com>
2026-07-21 11:30:53 -0600

    Attempt to fix github workflow

--- scripts/install-dependencies.lisp
+++ scripts/install-dependencies.lisp
@@ -17,12 +17,4 @@
         (uiop:quit 1)))
     (ql:quickload required)))
 
-(let* ((p (get-cur-filename))
-       (dir (reverse (cdr (reverse (pathname-directory p)))))
-       (path (make-pathname :directory dir :name
-			    "init-build-env" :type "lisp")))
-  (format *error-output* "Root project directory at ~S~%" dir)
-  (format *error-output* "Setting up environment...~%")
-  (load path))
-
 (ql-install-dependencies "cl-metabuild")
