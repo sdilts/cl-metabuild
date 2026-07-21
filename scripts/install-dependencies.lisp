f9493e2b4b57e04ef7f71da46bada0580a8821bb
Stuart Dilts <stuart.dilts@gmail.com>
2026-07-21 11:33:22 -0600

    Attempt to fix workflow

--- scripts/install-dependencies.lisp
+++ scripts/install-dependencies.lisp
@@ -17,4 +17,28 @@
         (uiop:quit 1)))
     (ql:quickload required)))
 
+(defun cur-dir ()
+  (make-pathname
+   :directory (pathname-directory (or *load-truename* *compile-file-pathname*))))
+
+(defun metabuild-system-dir ()
+  "Get the parent directory of this file. Will only work if this
+file is loaded or compiled, and not when interactively evaluated."
+  (let* ((cur-dir (cur-dir))
+		 (path-type (car (pathname-directory cur-dir)))
+		 (dir-list (cons path-type (reverse
+									(cdr (reverse
+										  (cdr (pathname-directory cur-dir))))))))
+	(make-pathname :directory dir-list)))
+
+(let ((src-registry (list :source-registry
+						  ;; Make the cl-metabuild system visible to the environment:
+						  (list :directory (metabuild-system-dir))
+						  ;; Make the example project visible to ASDF:
+						  (list :directory (cur-dir))
+						  :inherit-configuration)))
+  (finish-output)
+  (asdf:initialize-source-registry
+   src-registry))
+
 (ql-install-dependencies "cl-metabuild")
